c176f87dd1c06aaefcf79bf9958faf350c5a0ad5
hendrikpw <h.wanka2000@gmail.com>
2026-07-24 21:47:37 +0200

    refactor: share portfolio design in standalone app

--- air_quality_intelligence/app.py
+++ air_quality_intelligence/app.py
@@ -3,7 +3,9 @@
 import streamlit as st
 
 from air_quality_intelligence.ui import render_dashboard
+from portfolio_pages.design import inject_design_system
 
 
 st.set_page_config(page_title="European Air Quality Intelligence", page_icon="◎", layout="wide")
+inject_design_system()
 render_dashboard()
